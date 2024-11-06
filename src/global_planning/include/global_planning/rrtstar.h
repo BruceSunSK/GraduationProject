b3295bbf33e2b24f0642cb493335d4d854fe50a7
BruceSun <2357249789@qq.com>
2024-11-07 00:05:39 +0800

    对planner抽象基类补充注释；添加rrtstar的框架。

---
+++ src/global_planning/include/global_planning/rrtstar.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <random>
+
+#include "global_planning/global_planner_interface.h"
+#include "global_planning/tools/print_struct_and_enum.h"
+#include "global_planning/tools/path_simplification.h"
+
+
+class RRTstar : public GlobalPlannerInterface
+{
+
+};
